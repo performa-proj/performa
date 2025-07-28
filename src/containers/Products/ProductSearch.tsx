1f0e80d21c9561c1c5bbccc3ac3e84540abce069
Panit Tuangsuwan <tpanitte@hotmail.com>
2025-07-28 13:50:11 +0700

    feat: Develop Product Search in Orders

--- src/containers/Products/ProductSearch.tsx
+++ src/containers/Products/ProductSearch.tsx
@@ -26,7 +26,7 @@ export default function ProductSearch({
   return (
     <div className={classnames(
       isLoading ? "cursor-not-allowed bg-gray-100" : "bg-white",
-      "flex items-center rounded-md px-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-blue-600"
+      "flex items-center rounded-md pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-blue-600"
     )}>
       <div className="shrink-0 select-none text-sm/6 font-medium text-gray-600">Product SKU:</div>
       <input
@@ -47,10 +47,15 @@ export default function ProductSearch({
           }
         }}
       />
-      <MagnifyingGlassIcon
-        aria-hidden="true"
-        className="size-5 self-center justify-self-end text-gray-400 hover:text-gray-600"
-      />
+      <span
+        className="px-3 py-2"
+        onClick={() => handleSearch()}
+      >
+        <MagnifyingGlassIcon
+          aria-hidden="true"
+          className="size-5 self-center justify-self-end text-gray-400 hover:text-gray-600"
+        />
+      </span>
     </div>
   );
 }
