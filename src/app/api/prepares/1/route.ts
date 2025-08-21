f23b24e9f1e01fd018bc482a168ab108aa34bc3a
Panit Tuangsuwan <tpanitte@hotmail.com>
2025-08-21 21:15:12 +0700

    feat: Order PoS

--- src/app/api/prepares/1/route.ts
+++ src/app/api/prepares/1/route.ts
@@ -5,31 +5,31 @@ export const GET = async () => {
   await PriceStructures.createNew({
     title: Structures.Rices[0].title,
     cost: Structures.Rices[0].cost,
-    levels: Array(6).fill(0),
+    levels: Structures.Rices[0].levels,
   });
 
   await PriceStructures.createNew({
     title: Structures.Rices[1].title,
     cost: Structures.Rices[1].cost,
-    levels: Array(6).fill(0),
+    levels: Structures.Rices[1].levels,
   });
 
   await PriceStructures.createNew({
     title: Structures.Rices[2].title,
     cost: Structures.Rices[2].cost,
-    levels: Array(6).fill(0),
+    levels: Structures.Rices[2].levels,
   });
 
   await PriceStructures.createNew({
     title: Structures.Rices[3].title,
     cost: Structures.Rices[3].cost,
-    levels: Array(6).fill(0),
+    levels: Structures.Rices[3].levels,
   });
 
   await PriceStructures.createNew({
     title: Structures.Rices[4].title,
     cost: Structures.Rices[4].cost,
-    levels: Array(6).fill(0),
+    levels: Structures.Rices[4].levels,
   });
 
   return Response.json({
